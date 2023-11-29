2fde5f091850f79a9afd8faabb864dc2b72732b9
Mohammed Saif <msaif28@icloud.com>
2023-11-29 23:07:21 +0300

    after fixing parallel training part

--- scripts/train.py
+++ scripts/train.py
@@ -1,18 +1,21 @@
 import sys
 import os
-from tqdm import tqdm
-from sklearn.metrics import accuracy_score
 
+
+# Ensure the script can find custom modules
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+from tqdm import tqdm
 import torch
 import torch.nn as nn
+from torch.nn import DataParallel
 from utils.dataloader import load_data
 from models.alexnet import AlexNet
 from models.vgg import VGG
 from models.resnet import ResNet
 from models.efficientnet import EfficientNet
 from models.vit import ViT
+
 
 def select_model(model_name, num_classes):
     if model_name == 'AlexNet':
@@ -33,20 +36,25 @@ def train_model(model, train_loader, criterion, optimizer, num_epochs, model_nam
     os.makedirs(output_dir, exist_ok=True)
     print(f"Output directory is set to: {output_dir}")
 
+    # Move the model to the designated computing device
+    model = model.to(device)
+
+    # If more than one GPU is available, use DataParallel
     if torch.cuda.is_available() and torch.cuda.device_count() > 1:
         print(f"Using {torch.cuda.device_count()} GPUs!")
-        model = nn.DataParallel(model)
+        model = DataParallel(model)
 
-    model.to(device)
-    
     for epoch in range(num_epochs):
         model.train()
         total_loss = 0
         correct = 0
         total = 0
-        progress_bar = tqdm(enumerate(train_loader), total=len(train_loader), desc=f"Epoch {epoch+1}/{num_epochs}")
+        progress_bar = tqdm(enumerate(train_loader), total=len(train_loader), desc=f"Epoch {epoch + 1}/{num_epochs}")
+
         for i, (images, labels) in progress_bar:
-            images, labels = images.to(device), labels.to(device)
+            images = images.to(device)
+            labels = labels.to(device)
+
             optimizer.zero_grad()
             outputs = model(images)
             loss = criterion(outputs, labels)
@@ -58,12 +66,13 @@ def train_model(model, train_loader, criterion, optimizer, num_epochs, model_nam
             correct += (predicted == labels).sum().item()
             total += labels.size(0)
 
-            progress_bar.set_postfix(loss=total_loss/(i+1), accuracy=100. * correct / total)
+            progress_bar.set_postfix(loss=total_loss / (i + 1), accuracy=100. * correct / total)
 
         average_loss = total_loss / len(train_loader)
         accuracy = 100. * correct / total
-        print(f"Epoch {epoch+1} completed. Average Loss: {average_loss:.4f}. Accuracy: {accuracy:.2f}%.")
+        print(f"Epoch {epoch + 1} completed. Average Loss: {average_loss:.4f}. Accuracy: {accuracy:.2f}%.")
 
+    # Save the model
     model_save_path = os.path.join(output_dir, f"{model_name}_{num_epochs}_epochs.pth")
     try:
         torch.save(model.module.state_dict() if isinstance(model, nn.DataParallel) else model.state_dict(), model_save_path)
@@ -72,31 +81,27 @@ def train_model(model, train_loader, criterion, optimizer, num_epochs, model_nam
         print(f"Error saving the model: {e}")
 
 def main():
-    # Check GPU availability and list them
+    # Check GPU availability
     if torch.cuda.is_available():
         print("Available GPUs:", torch.cuda.device_count())
         for i in range(torch.cuda.device_count()):
             print("GPU", i, ":", torch.cuda.get_device_name(i))
-    # Set the default device for computations
+
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print(device)
-    model_name = "AlexNet"
-    num_classes = 55
+    print(f"Device set to: {device}")
+
+    model_name = "AlexNet"  # Replace with your chosen model
+    num_classes = 55  # Replace with the number of classes in your dataset
 
     data_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data', 'train'))
     train_loader = load_data(data_dir, batch_size=32)
-    
-    model = select_model(model_name, num_classes)
-    if torch.cuda.device_count() > 1:
-        print(f"Using {torch.cuda.device_count()} GPUs!")
-        model = nn.DataParallel(model)
 
-    model.to(device)
-    
-     # Define your criterion and optimizer
+    model = select_model(model_name, num_classes)
+    model = model.to(device)
+
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-    num_epochs = 10
+    num_epochs = 1000
 
     train_model(model, train_loader, criterion, optimizer, num_epochs, model_name, device)
 
